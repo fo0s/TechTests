75d7df46ef0b09329166ae3650b60590e92656bf
fo0s <fo0sies@gmail.com>
2019-07-15 11:53:45 +0100

    Add: Room builder with tests

--- Roomba/lib/roomba_room_builder.rb
+++ Roomba/lib/roomba_room_builder.rb
@@ -1,9 +1,37 @@
 # Start of the room builder class
-
 class RoomBuilder
-  attr_reader :output
+  attr_reader :output, :obstructions_used
 
   def initialize
-    @output = [] 
+    @output = []
+    @obstructions_used = false
+  end
+
+  def build_room(x, y, obstructions)
+    # Room will be 'x' long, +2 to factor in the walls
+    outer_wall = Array.new((x + 2), 'x')
+    obstructions_left = obstructions
+
+    @output << outer_wall
+
+    y.times do
+      temp_inner_wall = Array.new(y)
+      inner_wall = temp_inner_wall.map do |spot|
+        if rand(1..15) > 12 && obstructions_left != 0
+          obstructions_left -= 1
+          'O'
+        else
+          '*'
+        end
+
+      end
+
+      inner_wall.unshift('x').push('x')
+      @output << inner_wall
+    end
+
+    # The last wall
+    @output << outer_wall
+    @obstructions_used = true if obstructions_left == 0
   end
 end
--- Roomba/spec/roomba_room_builder_spec.rb
+++ Roomba/spec/roomba_room_builder_spec.rb
@@ -3,12 +3,45 @@ require 'roomba_room_builder'
 describe 'Room builder' do
   before(:each) do
     @room = RoomBuilder.new
+    @room.build_room(5, 5, 0)
   end
 
   context '#basics-- ' do
     it 'can start and hold an output value' do
-      expect(@room.output).to eq []
+      expect(@room.output).to_not eq []
     end
   end
+
+  context '#room structure-- ' do
+    it 'starts with the walls' do
+      expect(@room.output.first).to eq ['x', 'x', 'x', 'x', 'x', 'x', 'x']
+    end
+
+    it 'ends with the walls' do
+      expect(@room.output.last).to eq ['x', 'x', 'x', 'x', 'x', 'x', 'x']
+    end
+
+    it 'is surrounded with walls' do
+      expect(@room.output[2]).to include '*'
+    end
+
+    it 'is the right size' do
+      expect(@room.output.count).to eq 7
+    end
+
+    it 'randomly places obstructions in the room' do
+      @room.build_room(5, 5, 2)
+      expect(@room.obstructions_used).to eq true
+    end
+  end
+
+  # Expect build_room(5, 5) function to create a room;
+  # ['x', 'x', 'x', 'x', 'x', 'x', 'x']
+  # ['x', '*', '*', '*', '*', '*', 'x']
+  # ['x', '*', '*', '*', '*', '*', 'x']
+  # ['x', '*', '*', '*', '*', '*', 'x']
+  # ['x', '*', '*', '*', '*', '*', 'x']
+  # ['x', '*', '*', '*', '*', '*', 'x']
+  # ['x', 'x', 'x', 'x', 'x', 'x', 'x']
 
 end
