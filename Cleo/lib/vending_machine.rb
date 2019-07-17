9e401ce9cb6eaad922f45582ca479cfee3587aa2
fo0s <fo0sies@gmail.com>
2019-07-17 15:59:20 +0100

    Add: Can read json file
    1. Items are stored in a json file with the price and amount of items
    2. Class can read and parse json objects
    3. Can look up products

--- Cleo/lib/vending_machine.rb
+++ Cleo/lib/vending_machine.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 # Start of the main vending machine class
 
 class VendingMachine
@@ -7,5 +9,13 @@ class VendingMachine
   def initialize
     @data_source = ""
     @output = ""
+  end
+
+  def parse_json
+    JSON.parse(File.read(@data_source))
+  end
+
+  def item_exists?(item)
+    parse_json.key?(item)
   end
 end
--- Cleo/spec/vending_machine_spec.rb
+++ Cleo/spec/vending_machine_spec.rb
@@ -1,11 +1,21 @@
 # Main vending machine tests
 
 describe 'Vending machine' do
+  before(:all) { @test_data = 'data/products_test.json' }
   before(:each) { @vending_test = VendingMachine.new }
 
   context '#basics--' do
     it 'can start' do
       expect(@vending_test.output).to eq ""
+    end
+
+    it 'can store a data source' do
+      expect(@vending_test.data_source=(@test_data)).to eq @test_data
+    end
+
+    it 'can read a json file' do
+      @vending_test.data_source=(@test_data)
+      expect(@vending_test.item_exists?('Water')).to eq true
     end
   end
 end
