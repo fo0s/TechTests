a8c24bf776dbc69f5a4ccec179a6b83a193f5229
fo0s <fo0sies@gmail.com>
2019-07-23 15:53:24 +0100

    Add: Cleans and returns break times

--- Tenzo/bin/process_shifts.py
+++ Tenzo/bin/process_shifts.py
@@ -8,3 +8,23 @@ class Shifts:
                 return datetime.strptime(time_input, changed_time)
             except ValueError:
                 pass
+
+    def shift_start_time(self, input):
+        return self.clean_time(input[3])
+
+    def shift_finish_time(self, input):
+        return self.clean_time(input[1])
+
+    def separate_breaks(self, input, wanted):
+        break_time = input[0].split("-")
+        checking = 0
+
+        for time in break_time:
+            if 'PM' not in break_time[checking] and (int(break_time[checking]) - 12) < 0:
+                break_time[checking] += 'PM'
+            checking += 1
+
+        if wanted == 'start':
+            return self.clean_time(break_time[0])
+        else:
+            return self.clean_time(break_time[1])
