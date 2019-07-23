04417e4ad9d05718a823fa574ae932fe1afbaa3d
fo0s <fo0sies@gmail.com>
2019-07-23 12:20:04 +0100

    Add: Cleans time to universal

--- Tenzo/bin/process_shifts.py
+++ Tenzo/bin/process_shifts.py
@@ -3,7 +3,7 @@ from datetime import *
 
 class Shifts:
     def clean_time(self, time_input):
-        for changed_time in ('%H', '%H.%M', '%H:%M'):
+        for changed_time in ('%H', '%H.%M', '%H:%M', '%I%p', '%I.%M%p', '%H ', ' %H'):
             try:
                 return datetime.strptime(time_input, changed_time)
             except ValueError:
--- Tenzo/tests/shift_test.py
+++ Tenzo/tests/shift_test.py
@@ -3,8 +3,23 @@ from datetime import *
 from bin.process_shifts import Shifts
 
 
-shifts = Shifts()
+class TestTimes:
+    shifts = Shifts()
 
+    def test_time1(self):
+        assert self.shifts.clean_time('10') == datetime(1900, 1, 1, 10, 00)
 
-def test_time():
-    assert shifts.clean_time('10') == datetime(1900, 1, 1, 10, 00)
+    def test_time2(self):
+        assert self.shifts.clean_time('18:00') == datetime(1900, 1, 1, 18, 00)
+
+    def test_time3(self):
+        assert self.shifts.clean_time('4pm') == datetime(1900, 1, 1, 16, 00)
+
+    def test_time4(self):
+        assert self.shifts.clean_time('15 ') == datetime(1900, 1, 1, 15, 00)
+
+    def test_time5(self):
+        assert self.shifts.clean_time(' 17') == datetime(1900, 1, 1, 17, 00)
+
+    def test_time6(self):
+        assert self.shifts.clean_time('4.10PM') == datetime(1900, 1, 1, 16, 10)
